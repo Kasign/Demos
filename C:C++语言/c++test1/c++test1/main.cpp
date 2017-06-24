86dc6606f7accfdaee87349e207cd979aa4e7c86
Walg <393890129@qq.com>
2017-06-24 08:59:20 +0800

    c++

---
+++ C:C++语言/c++test1/c++test1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  c++test1
+//
+//  Created by Walg on 2017/6/21.
+//  Copyright © 2017年 Walg. All rights reserved.
+//
+
+#include <iostream>
+#include "aaa.cpp"
+#include "abc.cpp"
+
+int main(int argc, const char * argv[]) {
+    
+    
+  std::cout << "Hello, World!\n";
+    return 0;
+}
