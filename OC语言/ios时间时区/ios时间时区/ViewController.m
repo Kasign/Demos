717d8b272e99783e46ea06b78c5e527e5eb5c3a4
Wanghailong <wanghailong@ileci.com>
2017-08-15 17:45:07 +0800

    时区时间

--- OC语言/ios时间时区/ios时间时区/ViewController.m
+++ OC语言/ios时间时区/ios时间时区/ViewController.m
@@ -16,7 +16,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSDate *now = [NSDate date];
+    
+    NSDateFormatter *df = [[NSDateFormatter alloc]init];//时间戳格式化
+    
+    df.dateFormat = @"yyyy-MM-dd HH:mm:ss"; // 格式化年月日，时分秒
+    
+    df.timeZone = [NSTimeZone systemTimeZone];//系统所在时区
+    
+    NSString *systemTimeZoneStr =  [df stringFromDate:now];
+    
+    df.timeZone = [NSTimeZone defaultTimeZone];//默认时区，貌似和上一个没什么区别
+    NSString *defaultTimeZoneStr = [df stringFromDate:now];
+    
+    df.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0 * 3600];//直接指定时区
+    NSString *plus8TZStr = [df stringFromDate:now];
+    
+    df.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:-4*3600];//这就是GMT+0时区了
+    
+    NSString *gmtTZStr = [df stringFromDate:now];
+    
+    NSLog(@"Test Time\nSys:%@\nDefault:%@\n+8:%@\nGMT:%@",systemTimeZoneStr,defaultTimeZoneStr,plus8TZStr,gmtTZStr);
+
 }
 
 
