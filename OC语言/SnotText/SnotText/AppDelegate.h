9af829cf8329b9440ad94c1717c868cae3f2b682
Kasign <393890129@qq.com>
2019-03-13 22:35:10 +0800

    snot

---
+++ OC语言/SnotText/SnotText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnotText
+//
+//  Created by Walg on 2019/3/13.
+//  Copyright © 2019 Fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC语言/SnotText/SnotText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnotText
+//
+//  Created by Walg on 2019/3/13.
+//  Copyright © 2019 Fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
