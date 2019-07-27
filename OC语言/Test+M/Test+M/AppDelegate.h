857438d9f98d64b4060239dd2dc7cef3849282e2
Kasign <393890129@qq.com>
2019-07-27 16:05:38 +0800

    19.07.27

---
+++ OC语言/Test+M/Test+M/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test+M
+//
+//  Created by Walg on 2019/6/3.
+//  Copyright © 2019 Fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Swift语言/Test+Playground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "123"
+var a = 1
+print(112)
+
+if true {
+    print("Im ture",1,2,3, separator:"/")
+}
