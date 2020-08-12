bb55c2bfddc4538c54259191e5745fa4126486b6
Kasign <393890129@qq.com>
2020-08-12 16:52:43 +0800

    flutter

---
+++ OC语言/多种测试代码合集/算法+链表/KVC/NSObject+FlyKVC.h
@@ -0,0 +1,23 @@
+//
+//  NSObject+FlyKVC.h
+//  算法+链表
+//
+//  Created by Walg on 2020/3/8.
+//  Copyright © 2020 Fly. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (FlyKVC)
+
+- (void)fly_setValue:(id)value forKey:(id)key;
+- (id)fly_valueForKey:(id)key;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OC语言/多种测试代码合集/算法+链表/KVC/NSObject+FlyKVC.m
@@ -0,0 +1,26 @@
+//
+//  NSObject+FlyKVC.m
+//  算法+链表
+//
+//  Created by Walg on 2020/3/8.
+//  Copyright © 2020 Fly. All rights reserved.
+//
+
+#import "NSObject+FlyKVC.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation NSObject (FlyKVC)
+
+- (void)fly_setValue:(id)value forKey:(id)key {
+    
+    
+}
+
+- (id)fly_valueForKey:(id)key {
+    
+    return nil;
+}
+
+@end
