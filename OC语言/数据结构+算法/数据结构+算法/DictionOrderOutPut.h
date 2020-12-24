05911ff8f69763c1e6d8e9ccff5de712192a67f5
Kasign <393890129@qq.com>
2020-12-24 10:54:48 +0800

    字典序输出数字

---
+++ OC语言/数据结构+算法/数据结构+算法/DictionOrderOutPut.h
@@ -0,0 +1,28 @@
+//
+//  DictionOrderOutPut.h
+//  数据结构+算法
+//
+//  Created by Qiushan on 2020/12/24.
+//  Copyright © 2020 Fly. All rights reserved.
+//
+
+#ifndef DictionOrderOutPut_h
+#define DictionOrderOutPut_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ 有两个数字n和m，都是非负数，n > m, 按字典序输出所有1 - n中m个数字的组合
+ eg:
+ 输入：
+ 3 2
+ 输出：
+ 1 2
+ 1 3
+ 2 3
+ */
+
+void outPutNums(int n, int m);
+
+#endif /* DictionOrderOutPut_h */
