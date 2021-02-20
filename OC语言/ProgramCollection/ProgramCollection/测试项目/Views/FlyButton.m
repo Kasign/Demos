72c87f573ff3d2df6e5bc7053297156a8d56e503
Kasign <393890129@qq.com>
2021-02-20 11:04:16 +0800

    强引用

---
+++ OC语言/ProgramCollection/ProgramCollection/测试项目/Views/FlyButton.m
@@ -0,0 +1,18 @@
+//
+//  FlyButton.m
+//  ProgramCollection
+//
+//  Created by Qiushan on 2021/2/20.
+//  Copyright © 2021 FLY. All rights reserved.
+//
+
+#import "FlyButton.h"
+
+@implementation FlyButton
+
+- (void)dealloc {
+   
+    FLYLog(@"----* %@ dealloc *----", [self class]);
+}
+
+@end
